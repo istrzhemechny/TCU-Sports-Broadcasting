7caa7c5ebf5d9c7daca946ebe2c4c645ae09156f
Stefan Saba <stefan.saba@icloud.com>
2025-03-31 13:13:16 -0500

    first commit

---
+++ backend/TCU-sports-broadcasting/src/main/java/edu/tcu/cs/tcusportsbroadcasting/TcuSportsBroadcastingApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.tcusportsbroadcasting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcuSportsBroadcastingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcuSportsBroadcastingApplication.class, args);
+    }
+
+}
---
+++ backend/TCU-sports-broadcasting/src/test/java/edu/tcu/cs/tcusportsbroadcasting/TcuSportsBroadcastingApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.tcusportsbroadcasting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcuSportsBroadcastingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
