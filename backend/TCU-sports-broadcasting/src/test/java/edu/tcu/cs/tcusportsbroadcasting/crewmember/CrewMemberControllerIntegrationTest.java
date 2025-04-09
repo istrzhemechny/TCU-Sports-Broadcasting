9ef88aa09664bddaea8df00987b589aec160525b
Stefan Saba <stefan.saba@icloud.com>
2025-04-09 15:39:48 -0500

    update

--- backend/TCU-sports-broadcasting/src/test/java/edu/tcu/cs/tcusportsbroadcasting/crewmember/CrewMemberControllerIntegrationTest.java
+++ backend/TCU-sports-broadcasting/src/test/java/edu/tcu/cs/tcusportsbroadcasting/crewmember/CrewMemberControllerIntegrationTest.java
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class CrewMemberControllerIT {
+class CrewMemberControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
