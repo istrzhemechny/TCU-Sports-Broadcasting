5411aa43233967a5618e8591510b1f51e5481b74
Stefan Saba <stefan.saba@icloud.com>
2025-04-28 13:07:57 -0500

    cors config update

--- backend/TCU-sports-broadcasting/src/main/java/edu/tcu/cs/tcusportsbroadcasting/security/CorsConfiguration.java
+++ backend/TCU-sports-broadcasting/src/main/java/edu/tcu/cs/tcusportsbroadcasting/security/CorsConfiguration.java
@@ -13,7 +13,7 @@ public class CorsConfiguration {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**").allowedMethods("DELETE", "GET", "POST", "PUT");
             }
         };
     }
